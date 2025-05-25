7c7238d7e73af07f5e84a8e8842a795388e9ca44
JSackey101 <jeffsackey101@gmail.com>
2025-05-25 15:08:21 +0100

    Updated players class

--- players/players.py
+++ players/players.py
@@ -5,9 +5,27 @@ class Player:
     
     def __init__(self, nickname: str, name: str, element: str,
                  gender: str, position: list[str], school_year: str) -> None:
-        self.nickname = nickname
-        self.name = name
-        self.element = element
-        self.gender = gender
-        self.position = position
-        self.school_year = school_year
+        self.nickname = nickname.lower()
+        self.name = name.lower()
+        self.element = element.lower()
+        self.gender = gender.lower()
+        self.position = [pos.upper() for pos in position]
+        self.school_year = school_year.upper()
+    
+    def __str__(self) -> str:
+        """ Human-Readable String Representation """
+        return f"{self.nickname.title()} ({self.name.title()})"
+    
+    def __eq__(self, other: "Player") -> bool:
+        """ Checks for equality between 2 Player objects. """
+        return all(self.compare_players(other))
+
+    def compare_players(self, other_player: "Player") -> list[bool]:
+        """ Returns a list containing True/False representing the comparison
+            of 2 Player Objects"""
+        return [self.nickname == other_player.nickname,
+                self.name == other_player.name,
+                self.element == other_player.element,
+                self.gender == other_player.gender,
+                self.position == other_player.position,
+                self.school_year == other_player.school_year]
