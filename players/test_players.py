4055a1259dcdb2b3c83f21abbc1041d28eb1ce38
JSackey101 <jeffsackey101@gmail.com>
2025-05-27 09:00:32 +0100

    Set up tests I wish to write.

---
+++ players/test_players.py
@@ -0,0 +1,25 @@
+""" """
+
+class TestPlayers():
+    """ Class to test the Player Class. """
+    
+    @staticmethod
+    def test_player_creation():
+        """ Test the creation of a Player object. """
+        pass
+    
+    @staticmethod
+    def test_equality():
+        """ Test that Player objects are equal. """
+        pass
+        
+    @staticmethod
+    def test_inequality():
+        """ Test that Player objects are not equal. """
+        pass
+        
+    @staticmethod
+    def test_compare_players():
+        """ Test that comparison list returned is correct. """
+        pass
+    
